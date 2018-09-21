704befd7654dfac88ad13a2dca4ad5bb58287db7
James Hwang <91juhwang@gmail.com>
2018-09-21 16:58:54 -0400

    Initial commit

--- lib/sentence_finder.rb
+++ lib/sentence_finder.rb
@@ -1,6 +1,30 @@
 module SentenceFinder
+  ALPHABETS = ('a'..'z').to_a.join
+
   def self.find(paragraph)
     # Let's make this work!
-    paragraph
+    sentences = paragraph.scan(/[^\.!?]+[\.!?]/)
+    array_of_words = sentences.map { |sentence| sentence.split(' ')}
+
+    a = []
+    array_of_words.each do |array|
+      array.each do |word|
+        string_one = word.chars.sort_by(&:downcase).join
+        puts string_one
+        consecutive_words = string_one.each_char.each_cons(2).find_all {|letters| ALPHABETS.include? (letters.join) }
+        a << consecutive_words
+      end
+      a
+    end
+    puts a
+    # alphabetical_order_words = array_of_words.map { |c| c.sort_by(&:downcase) }
+
+    # iterate over sentences
+    # for each sentence split them into words
+    # sort each words alphabeetically
+    # Compare with alphabets
+    # count how many words contain consecutive letters
+
   end
 end
+# 'test'.each_char.each_cons(2).find_all {|letters| alphabets.join('').include? (letters.join) }
--- spec/sentence_finder_spec.rb
+++ spec/sentence_finder_spec.rb
@@ -9,6 +9,15 @@ describe SentenceFinder do
       end
     end
 
-    it 'can find a sentence with an alpha-consecutive word'
+    context 'when a paragraph with two sentences are given' do
+      it 'returns the first sentence with most alphabetical-consecutive word' do
+        sentence = SentenceFinder.find('Two sentences with the same numbers of alphabet-consecutive words. Both sentences have five words which are consecutive and that is it')
+        expect(sentence).to eq('Two sentences with the same numbers of alphabet-consecutive words.')
+      end
+    end
+
+    it 'can find a sentence with an alpha-consecutive word' do
+
+    end
   end
 end
